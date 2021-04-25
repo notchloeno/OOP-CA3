5d9aae664256d9a5c5b79c3b075b5893de7b89d9
Josh <jr768@exeter.ac.uk>
2021-04-25 16:57:54 +0100

    Add methods to SocialMedia and Account

--- src/socialmedia/Account.java
+++ src/socialmedia/Account.java
@@ -1,6 +1,9 @@
 package socialmedia;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Account {
 
@@ -10,39 +13,68 @@ public class Account {
     private String description;  // "Bio" of the account
     private List<Post> posts;  // All the posts (including comments and endorsements) associated with this Account
 
+    // Constructors
+
+    public Account(String handle)
+            throws InvalidHandleException {
+        this(handle, null);
+    }
+
+    public Account(String handle, String description)
+            throws InvalidHandleException{
+
+        verifyHandle(handle);
+
+        this.accountID = nextID++;  // Set the account ID and then increment it
+        this.handle = handle;
+        this.description = description;
+    }
+
+    // Getters/Setters
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle)  throws InvalidHandleException{
+        verifyHandle(handle);
+        this.handle = handle;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Method
+
     // Verify if the handle is legal
-    private boolean verifyHandle(String handle) throws IllegalHandleException, InvalidHandleException {
+    private void verifyHandle(@NotNull String handle) throws InvalidHandleException {
         // Check if the handle  is valid
         int handleLength = handle.length();
         if (handleLength < 1 || 30 < handleLength){
             throw new InvalidHandleException("Handle must be between 1 and 30 characters");
         }
-        // Check the handle hasn't been used already
-        return true;
+        if (handle.contains(" ")){
+            throw new InvalidHandleException("Handle must not contain any spaces");
+        }
     }
 
-    private boolean verifyDescription(String description){
-        return true;
+    public int getPostCount(){
+        return posts.size();
     }
 
-    public Account(String handle, String description)
-            throws IllegalHandleException, InvalidHandleException{
-
-        try
-        {
-            verifyHandle(handle);
-            verifyDescription(description);
-        }
-        catch (IllegalHandleException e)
-        {
-            throw e;
-        }
-        catch (InvalidHandleException e){
-            throw e;
-        }
-
-        this.accountID = nextID++;  // Set the account ID and then increment it
-        this.handle = handle;
-        this.description = description;
+    public int getEndorsementCount() {
+        return (int) posts
+                .stream()
+                .filter(p -> p instanceof Endorsement)
+                .count();
     }
 }
--- src/socialmedia/SocialMedia.java
+++ src/socialmedia/SocialMedia.java
@@ -1,6 +1,7 @@
 package socialmedia;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * SocialMedia is the functioning implementor of
@@ -12,46 +13,95 @@ import java.io.IOException;
  */
 public class SocialMedia implements SocialMediaPlatform {
 
+	private List<Account> accountList;
+	private List<Post> postList;
+
+	public Account getAccount(String handle){
+		for (Account account : accountList){
+			if (account.getHandle().equals(handle)){
+				return account;
+			}
+		}
+		return null;
+	}
+
+	public Account getAccount(int accountID){
+		for (Account account : accountList){
+			if (account.getAccountID() == accountID){
+				return account;
+			}
+		}
+		return null;
+	}
+
 	@Override
 	public int createAccount(String handle) throws IllegalHandleException, InvalidHandleException {
-		// TODO Auto-generated method stub
-		return 0;
+        createAccount(handle, null);
+        return 0;
 	}
 
 	@Override
 	public int createAccount(String handle, String description) throws IllegalHandleException, InvalidHandleException {
-		// TODO Auto-generated method stub
+		if (getAccount(handle) != null){
+			throw new IllegalHandleException("This handle is already associated with another account");
+		}
+		Account newAccount = new Account(handle);
+		accountList.add(newAccount);
 		return 0;
 	}
 
 	@Override
 	public void removeAccount(int id) throws AccountIDNotRecognisedException {
-		// TODO Auto-generated method stub
-
+		Account account = getAccount(id);
+		if (account == null) { throw new AccountIDNotRecognisedException();}
+		removeAccount(account);
 	}
 
 	@Override
 	public void removeAccount(String handle) throws HandleNotRecognisedException {
-		// TODO Auto-generated method stub
+		Account account = getAccount(handle);
+		if (account == null) { throw new HandleNotRecognisedException();}
+		removeAccount(account);
+	}
+
+	public void removeAccount(Account account){
 
 	}
 
 	@Override
 	public void changeAccountHandle(String oldHandle, String newHandle)
 			throws HandleNotRecognisedException, IllegalHandleException, InvalidHandleException {
-		// TODO Auto-generated method stub
-
+	    Account account = getAccount(oldHandle);
+	    if (account == null) {
+	    	throw new HandleNotRecognisedException("Account with old handle cannot be found"); }
+	    if (getAccount(newHandle) != null) {
+	    	throw new IllegalHandleException("New handle is already associated with another account"); }
+	    account.setHandle(newHandle);
 	}
 
 	@Override
 	public void updateAccountDescription(String handle, String description) throws HandleNotRecognisedException {
-		// TODO Auto-generated method stub
-
+		Account account = getAccount(handle);
+		if (account == null) {
+			throw new HandleNotRecognisedException("Account with handle cannot be found");
+		}
+		account.setDescription(description);
 	}
 
 	@Override
 	public String showAccount(String handle) throws HandleNotRecognisedException {
-		// TODO Auto-generated method stub
+	    Account account = getAccount(handle);
+	    if (account == null){
+	    	throw new HandleNotRecognisedException("Account with handle cannot be found");
+		}
+		System.out.println("ID:" + String.valueOf(account.getAccountID()));
+	    System.out.println("Handle: " + handle);
+	    String description = account.getDescription();
+	    if (description != null) {
+			System.out.println("Description:" + account.getDescription());
+		}
+	    System.out.println("Post count:" + String.valueOf(account.getPostCount()));
+	    System.out.println("Endorse count:" + String.valueOf(account.getEndorsementCount()));
 		return null;
 	}
 
